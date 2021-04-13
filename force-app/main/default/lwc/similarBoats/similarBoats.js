772dd58b996bf4a451834e63f07d699202fd1130
Nikola Filip <nikola.filip4@gmail.com>
2021-04-13 14:51:51 +0200

    Added similarBoats component

---
+++ force-app/main/default/lwc/similarBoats/similarBoats.js
@@ -0,0 +1,47 @@
+import { LightningElement, api, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getSimilarBoats from '@salesforce/apex/BoatDataService.getSimilarBoats';
+
+export default class SimilarBoats extends NavigationMixin(LightningElement) {
+	currentBoat;
+	relatedBoats;
+	boatId;
+	error;
+
+	@api get recordId() {
+		// returns the boatId
+		return this.boatId;
+	}
+	set recordId(value) {
+		this.boatId = value;
+	}
+
+	@api similarBy;
+
+	@wire(getSimilarBoats, { boatId: '$boatId', similarBy: '$similarBy' })
+	similarBoats({ error, data }) {
+		if (data) {
+			this.relatedBoats = data;
+		}
+		if (error) {
+			this.error = error;
+		}
+	}
+
+	get getTitle() {
+		return 'Similar boats by ' + this.similarBy;
+	}
+	get noBoats() {
+		return !(this.relatedBoats && this.relatedBoats.length > 0);
+	}
+
+	openBoatDetailPage(event) {
+		this[NavigationMixin.Navigate]({
+			type: 'standard__recordPage',
+			attributes: {
+				recordId: event.detail.boatId,
+				actionName: 'view'
+			}
+		});
+	}
+}
